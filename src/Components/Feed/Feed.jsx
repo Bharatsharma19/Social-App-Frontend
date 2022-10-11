8d5f5c7468a08aae8c1f198d00e89fa314389f52
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-11 22:15:12 +0530

    Share Component Updated

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Share from '../Share/Share'
 import './feed.css'
 
 const Feed = () => {
     return (
-        <div className='feed'>Feed</div>
+        <div className='feed'>
+            <div className="feedWrapper">
+                <Share />
+            </div>
+        </div>
     )
 }
 
