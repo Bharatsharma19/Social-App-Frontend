173fbe7adf7749efdbfbcab7fdeb3746ca600fec
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-11 20:51:30 +0530

    Topbar Completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Topbar from '../../Components/Topbar/Topbar'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Topbar />
+        </>
     )
 }
 
