b27accf388c9664e3507ff83b845b751e3ba5322
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-11 23:38:43 +0530

    Like Handler Added

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,9 +1,17 @@
 import { MoreVert } from '@mui/icons-material'
-import React from 'react'
+import { React, useState } from 'react'
 import './post.css'
 import { Users } from '../../dummyData'
 
 const Post = ({ post }) => {
+    const [like, setLike] = useState(post.like)
+    const [isLiked, setIsLiked] = useState(false)
+
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1)
+        setIsLiked(!isLiked)
+    }
+
     return (
         <div className='post'>
             <div className="postWrapper">
@@ -23,9 +31,9 @@ const Post = ({ post }) => {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="assets/like.png" alt="" />
-                        <img className="likeIcon" src="assets/heart.png" alt="" />
-                        <span className="postLikeCounter">{post.like} people like it</span>
+                        <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
+                        <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
+                        <span className="postLikeCounter">{like} people liked it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">{post.comment} comments</span>
