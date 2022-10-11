396da783d6910c7ce6ad71c0a94015e64a8aecb9
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-11 23:48:36 +0530

    More Pages Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Register />
     </div>
   );
 }
---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './profile.css'
+import Topbar from "../../Components/Topbar/Topbar";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Feed from "../../Components/Feed/Feed";
+import Rightbar from "../../Components/Rightbar/Rightbar";
+
+const Profile = () => {
+    return (
+        <>
+            <Topbar />
+            <div className="profile">
+                <Sidebar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img
+                                className="profileCoverImg"
+                                src="assets/post/3.jpeg"
+                                alt=""
+                            />
+                            <img
+                                className="profileUserImg"
+                                src="assets/person/7.jpeg"
+                                alt=""
+                            />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Vivek Sharma</h4>
+                            <span className="profileInfoDesc">Hello my friends!</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <Rightbar profile />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile
