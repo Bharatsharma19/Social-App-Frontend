9d9e3e3487f89ff394049566347f31b126da81d8
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-12 21:59:01 +0530

    Rightbar Updated

--- src/Components/Rightbar/Rightbar.jsx
+++ src/Components/Rightbar/Rightbar.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import './rightbar.css'
-import { Users } from '../../dummyData'
-import Online from '../Online/Online'
+import "./rightbar.css";
+import { Users } from "../../dummyData";
+import Online from "../Online/Online";
 
-const Rightbar = () => {
-    return (
-        <div className='rightbar'>
-            <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+    const HomeRightbar = () => {
+        return (
+            <>
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="assets/gift.png" alt="" />
                     <span className="birthdayText">
@@ -16,13 +15,91 @@ const Rightbar = () => {
                 <img className="rightbarAd" src="assets/ad.png" alt="" />
                 <h4 className="rightbarTitle">Online Friends</h4>
                 <ul className="rightbarFriendList">
-                    {Users.map(u => (
-                        < Online key={u.id} user={u} />
+                    {Users.map((u) => (
+                        <Online key={u.id} user={u} />
                     ))}
                 </ul>
+            </>
+        );
+    };
+
+    const ProfileRightbar = () => {
+        return (
+            <>
+                <h4 className="rightbarTitle">User information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">Gwalior</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From:</span>
+                        <span className="rightbarInfoValue">Malanpur</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship:</span>
+                        <span className="rightbarInfoValue">Single</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img
+                            src="assets/person/1.jpeg"
+                            alt=""
+                            className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Shivam Sharma</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                            src="assets/person/2.jpeg"
+                            alt=""
+                            className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Shivam Sharma</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                            src="assets/person/3.jpeg"
+                            alt=""
+                            className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Shivam Sharma</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                            src="assets/person/4.jpeg"
+                            alt=""
+                            className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Shivam Sharma</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                            src="assets/person/5.jpeg"
+                            alt=""
+                            className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Shivam Sharma</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                            src="assets/person/6.jpeg"
+                            alt=""
+                            className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Shivam Sharma</span>
+                    </div>
+                </div>
+            </>
+        );
+    };
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                {profile ? <ProfileRightbar /> : <HomeRightbar />}
             </div>
         </div>
-    )
+    );
 }
-
-export default Rightbar
