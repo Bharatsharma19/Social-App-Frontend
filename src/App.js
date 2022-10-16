118470e0bab827f52121c6a7eb4c538e5af81035
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-16 18:55:28 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, redirect } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Profile from "./Pages/Profile/Profile";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? redirect("/") : <Login />} />
+        <Route path="/register" element={user ? redirect("/") : <Register />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </BrowserRouter>
